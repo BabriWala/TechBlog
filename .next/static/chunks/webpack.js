ba396eaec9e4f60d34db52dfa11907bc05d42453
babriwala <hanzalafarhann@gmail.com>
2023-08-12 13:49:22 +0600

    problem solved

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ff1367b7aa995e11"; }
+/******/ 		__webpack_require__.h = function() { return "0a27b379b50b112d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Topics/Topics.jsx
+++ components/Topics/Topics.jsx
@@ -3,7 +3,7 @@ import styles from "./topics.module.css";
 
 const Topics = () => {
   return (
-    <section className={`${styles["topics-section"]} container `}>
+    <section className={`${styles["topics-section"]} container`}>
       <h2 className={styles["topics__section__title"]}>Popular Topics</h2>
       <div className={styles["topics-tags-container"]}>
         <span className={styles["topics-tags"]}>
@@ -13,184 +13,250 @@ const Topics = () => {
           <span>React</span>
           <span>React</span>
         </span>
-        <span className={`${styles.see-all}`}>View All</span>
+        <span className={styles["see-all"]}>View All</span>
       </div>
-      <div className={`${styles.topics-container}`}>
-        <div className={`${styles.topics-items-container}`}>
-          <div className={`${styles.topics-image-container}`}>
+      <div className={styles["topics-container"]}>
+        <div className={styles["topics-items-container"]}>
+          <div className={styles["topics-image-container"]}>
             <img
               src="https://images.pexels.com/photos/11035363/pexels-photo-11035363.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt=""
             />
             <span>JavaScript</span>
           </div>
-          <h6 className={`${styles.topics-items__date}`}>03.03.2003</h6>
-          <h4 className={`${styles.topics-items__title}`}>
+          <h6 className={styles["topics-items__date"]}>03.03.2003</h6>
+          <h4 className={styles["topics-items__title"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
             fuga?
           </h4>
-          <p className={`${styles.topics-items__description}`}>
+          <p className={styles["topics-items__description"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
             quaerat ea illum perspiciatis laborum, eaque aliquid aspernatur
             ipsa, assumenda repellendus laudantium accusamus blanditiis sit
             earum. Officiis quidem numquam alias nostrum.
           </p>
           <div></div>
-          <button className={`${styles.btn topics__btn}`}>Read More</button>
+          <button className={styles["btn topics__btn"]}>Read More</button>
         </div>
-        <div className={`${styles.topics-items-container}`}>
-          <div className={`${styles.topics-image-container}`}>
+        <div className={styles["topics-items-container"]}>
+          <div className={styles["topics-image-container"]}>
             <img
               src="https://images.pexels.com/photos/11035363/pexels-photo-11035363.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt=""
             />
             <span>JavaScript</span>
           </div>
-          <h6 className={`${styles.topics-items__date}`}>03.03.2003</h6>
-          <h4 className={`${styles.topics-items__title}`}>
+          <h6 className={styles["topics-items__date"]}>03.03.2003</h6>
+          <h4 className={styles["topics-items__title"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
             fuga?
           </h4>
-          <p className={`${styles.topics-items__description}`}>
+          <p className={styles["topics-items__description"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
             quaerat ea illum perspiciatis laborum, eaque aliquid aspernatur
             ipsa, assumenda repellendus laudantium accusamus blanditiis sit
             earum. Officiis quidem numquam alias nostrum.
           </p>
           <div></div>
-          <button className={`${styles.btn topics__btn}`}>Read More</button>
+          <button className={styles["btn topics__btn"]}>Read More</button>
         </div>
-        <div className={`${styles.topics-items-container}`}>
-          <div className={`${styles.topics-image-container}`}>
+        <div className={styles["topics-items-container"]}>
+          <div className={styles["topics-image-container"]}>
             <img
               src="https://images.pexels.com/photos/11035363/pexels-photo-11035363.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt=""
             />
             <span>JavaScript</span>
           </div>
-          <h6 className={`${styles.topics-items__date}`}>03.03.2003</h6>
-          <h4 className={`${styles.topics-items__title}`}>
+          <h6 className={styles["topics-items__date"]}>03.03.2003</h6>
+          <h4 className={styles["topics-items__title"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
             fuga?
           </h4>
-          <p className={`${styles.topics-items__description}`}>
+          <p className={styles["topics-items__description"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
             quaerat ea illum perspiciatis laborum, eaque aliquid aspernatur
             ipsa, assumenda repellendus laudantium accusamus blanditiis sit
             earum. Officiis quidem numquam alias nostrum.
           </p>
           <div></div>
-          <button className={`${styles.btn topics__btn}`}>Read More</button>
+          <button className={styles["btn topics__btn"]}>Read More</button>
         </div>
-        <div className={`${styles.topics-items-container}`}>
-          <div className={`${styles.topics-image-container}`}>
+        <div className={styles["topics-items-container"]}>
+          <div className={styles["topics-image-container"]}>
             <img
               src="https://images.pexels.com/photos/11035363/pexels-photo-11035363.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt=""
             />
             <span>JavaScript</span>
           </div>
-          <h6 className={`${styles.topics-items__date}`}>03.03.2003</h6>
-          <h4 className={`${styles.topics-items__title}`}>
+          <h6 className={styles["topics-items__date"]}>03.03.2003</h6>
+          <h4 className={styles["topics-items__title"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
             fuga?
           </h4>
-          <p className={`${styles.topics-items__description}`}>
+          <p className={styles["topics-items__description"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
             quaerat ea illum perspiciatis laborum, eaque aliquid aspernatur
             ipsa, assumenda repellendus laudantium accusamus blanditiis sit
             earum. Officiis quidem numquam alias nostrum.
           </p>
           <div></div>
-          <button className={`${styles.btn topics__btn}`}>Read More</button>
+          <button className={styles["btn topics__btn"]}>Read More</button>
         </div>
-        <div className={`${styles.topics-items-container}`}>
-          <div className={`${styles.topics-image-container}`}>
+        <div className={styles["topics-items-container"]}>
+          <div className={styles["topics-image-container"]}>
             <img
               src="https://images.pexels.com/photos/11035363/pexels-photo-11035363.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt=""
             />
             <span>JavaScript</span>
           </div>
-          <h6 className={`${styles.topics-items__date}`}>03.03.2003</h6>
-          <h4 className={`${styles.topics-items__title}`}>
+          <h6 className={styles["topics-items__date"]}>03.03.2003</h6>
+          <h4 className={styles["topics-items__title"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
             fuga?
           </h4>
-          <p className={`${styles.topics-items__description}`}>
+          <p className={styles["topics-items__description"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
             quaerat ea illum perspiciatis laborum, eaque aliquid aspernatur
             ipsa, assumenda repellendus laudantium accusamus blanditiis sit
             earum. Officiis quidem numquam alias nostrum.
           </p>
           <div></div>
-          <button className={`${styles.btn topics__btn}`}>Read More</button>
+          <button className={styles["btn topics__btn"]}>Read More</button>
         </div>
-        <div className={`${styles.topics-items-container}`}>
-          <div className={`${styles.topics-image-container}`}>
+        <div className={styles["topics-items-container"]}>
+          <div className={styles["topics-image-container"]}>
             <img
               src="https://images.pexels.com/photos/11035363/pexels-photo-11035363.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt=""
             />
             <span>JavaScript</span>
           </div>
-          <h6 className={`${styles.topics-items__date}`}>03.03.2003</h6>
-          <h4 className={`${styles.topics-items__title}`}>
+          <h6 className={styles["topics-items__date"]}>03.03.2003</h6>
+          <h4 className={styles["topics-items__title"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
             fuga?
           </h4>
-          <p className={`${styles.topics-items__description}`}>
+          <p className={styles["topics-items__description"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
             quaerat ea illum perspiciatis laborum, eaque aliquid aspernatur
             ipsa, assumenda repellendus laudantium accusamus blanditiis sit
             earum. Officiis quidem numquam alias nostrum.
           </p>
           <div></div>
-          <button className={`${styles.btn topics__btn}`}>Read More</button>
+          <button className={styles["btn topics__btn"]}>Read More</button>
         </div>
-        <div className={`${styles.topics-items-container}`}>
-          <div className={`${styles.topics-image-container}`}>
+        <div className={styles["topics-items-container"]}>
+          <div className={styles["topics-image-container"]}>
             <img
               src="https://images.pexels.com/photos/11035363/pexels-photo-11035363.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt=""
             />
             <span>JavaScript</span>
           </div>
-          <h6 className={`${styles.topics-items__date}`}>03.03.2003</h6>
-          <h4 className={`${styles.topics-items__title}`}>
+          <h6 className={styles["topics-items__date"]}>03.03.2003</h6>
+          <h4 className={styles["topics-items__title"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
             fuga?
           </h4>
-          <p className={`${styles.topics-items__description}`}>
+          <p className={styles["topics-items__description"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
             quaerat ea illum perspiciatis laborum, eaque aliquid aspernatur
             ipsa, assumenda repellendus laudantium accusamus blanditiis sit
             earum. Officiis quidem numquam alias nostrum.
           </p>
           <div></div>
-          <button className={`${styles.btn topics__btn}`}>Read More</button>
+          <button className={styles["btn topics__btn"]}>Read More</button>
         </div>
-        <div className={`${styles.topics-items-container}`}>
-          <div className={`${styles.topics-image-container}`}>
+        <div className={styles["topics-items-container"]}>
+          <div className={styles["topics-image-container"]}>
             <img
               src="https://images.pexels.com/photos/11035363/pexels-photo-11035363.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt=""
             />
             <span>JavaScript</span>
           </div>
-          <h6 className={`${styles.topics-items__date}`}>03.03.2003</h6>
-          <h4 className={`${styles.topics-items__title}`}>
+          <h6 className={styles["topics-items__date"]}>03.03.2003</h6>
+          <h4 className={styles["topics-items__title"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
             fuga?
           </h4>
-          <p className={`${styles.topics-items__description}`}>
+          <p className={styles["topics-items__description"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
             quaerat ea illum perspiciatis laborum, eaque aliquid aspernatur
             ipsa, assumenda repellendus laudantium accusamus blanditiis sit
             earum. Officiis quidem numquam alias nostrum.
           </p>
           <div></div>
-          <button className={`${styles.btn topics__btn}`}>Read More</button>
+          <button className={styles["btn topics__btn"]}>Read More</button>
+        </div>
+        <div className={styles["topics-items-container"]}>
+          <div className={styles["topics-image-container"]}>
+            <img
+              src="https://images.pexels.com/photos/11035363/pexels-photo-11035363.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+            />
+            <span>JavaScript</span>
+          </div>
+          <h6 className={styles["topics-items__date"]}>03.03.2003</h6>
+          <h4 className={styles["topics-items__title"]}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+            fuga?
+          </h4>
+          <p className={styles["topics-items__description"]}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+            quaerat ea illum perspiciatis laborum, eaque aliquid aspernatur
+            ipsa, assumenda repellendus laudantium accusamus blanditiis sit
+            earum. Officiis quidem numquam alias nostrum.
+          </p>
+          <div></div>
+          <button className={styles["btn topics__btn"]}>Read More</button>
+        </div>
+        <div className={styles["topics-items-container"]}>
+          <div className={styles["topics-image-container"]}>
+            <img
+              src="https://images.pexels.com/photos/11035363/pexels-photo-11035363.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+            />
+            <span>JavaScript</span>
+          </div>
+          <h6 className={styles["topics-items__date"]}>03.03.2003</h6>
+          <h4 className={styles["topics-items__title"]}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+            fuga?
+          </h4>
+          <p className={styles["topics-items__description"]}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+            quaerat ea illum perspiciatis laborum, eaque aliquid aspernatur
+            ipsa, assumenda repellendus laudantium accusamus blanditiis sit
+            earum. Officiis quidem numquam alias nostrum.
+          </p>
+          <div></div>
+          <button className={styles["btn topics__btn"]}>Read More</button>
+        </div>
+        <div className={styles["topics-items-container"]}>
+          <div className={styles["topics-image-container"]}>
+            <img
+              src="https://images.pexels.com/photos/11035363/pexels-photo-11035363.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+            />
+            <span>JavaScript</span>
+          </div>
+          <h6 className={styles["topics-items__date"]}>03.03.2003</h6>
+          <h4 className={styles["topics-items__title"]}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+            fuga?
+          </h4>
+          <p className={styles["topics-items__description"]}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+            quaerat ea illum perspiciatis laborum, eaque aliquid aspernatur
+            ipsa, assumenda repellendus laudantium accusamus blanditiis sit
+            earum. Officiis quidem numquam alias nostrum.
+          </p>
+          <div></div>
+          <button className={styles["btn topics__btn"]}>Read More</button>
         </div>
       </div>
     </section>
