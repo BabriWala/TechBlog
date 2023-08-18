1b0864ad0ebe31f3043b7d5604f378dcb6a749ac
babriwala <hanzalafarhann@gmail.com>
2023-08-19 01:12:25 +0600

    signup page added & cursor updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "02257e41c5e82672"; }
+/******/ 		__webpack_require__.h = function() { return "0b9cfb00950a3fe4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
