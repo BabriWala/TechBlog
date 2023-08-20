956ead60dbbf9c36beff40060931b0d3843833cf
babriwala <hanzalafarhann@gmail.com>
2023-08-20 12:46:19 +0600

    post schema ready

---
+++ src/models/post.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const {Schema} = mongoose;
+
+const postSchema = new Schema({
+    title: {
+        type: string,
+        required: true
+    },
+    description: {
+        type: string,
+        required: true
+    },
+    img: {
+        type: string,
+        required: true
+    },
+    content: {
+        type: string,
+        required: true
+    },
+    username: {
+        type: string,
+        required: true
+    }},
+    {timestamps: true}
+)
+
+export default mongoose.model("Post", postSchema);
